463f0fa8571cbcd31dd965de66d8cf18165568ce
kimjunho <wnsghi1@naver.com>
2021-03-23 10:55:00 +0900

    JPA 주문 내역 조회 > Entity 직접 노출 방법 - V1

--- src/main/java/jpabook/jpashop/JpashopApplication.java
+++ src/main/java/jpabook/jpashop/JpashopApplication.java
@@ -1,11 +1,26 @@
 package jpabook.jpashop;
 
+import com.fasterxml.jackson.datatype.hibernate5.Hibernate5Module;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class JpashopApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(JpashopApplication.class, args);
+	}
+
+	/**
+	 * Practice 를 위한 Dependency 추가 > Response Object 를 별도로 생성하여 관리할 경우, 아래 Hibernate5Module 필요 없음
+	 * 	=> LazyLoading 을 선언한 필드를 조회 하기 위함
+	 */
+
+	@Bean
+	Hibernate5Module hibernate5Module() {
+		// 아래와 같이 FORCE_LAZY_LOADING 을 통해 LAZY 이슈를 해결하여 Entity 를 반환하기 보다는 Response 용 DTO 를 생성하여 관리하는 것이 더 나은 설계
+		Hibernate5Module hibernate5Module = new Hibernate5Module();
+		// hibernate5Module.configure(Hibernate5Module.Feature.FORCE_LAZY_LOADING, true);
+		return hibernate5Module;
 	}
 }
--- src/main/java/jpabook/jpashop/api/OrderSimpleApiController.java
+++ src/main/java/jpabook/jpashop/api/OrderSimpleApiController.java
@@ -30,12 +30,20 @@ public class OrderSimpleApiController {
      *      => 아래와 같이 Entity 를 Response 로 사용하고자 한다면 둘 중 하나는 @JsonIgnore 처리 해주어야 함
      *      => 하지만, Entity 를 Response 로 사용하는 것은 API 스펙에 종속적이게 되므로 별도의 Response 를 만드는 것이 좋음
      *      => 이외 양방향 매핑으로 인하여 무한 루프를 발생시키는 필드들은 한쪽은 @JsonIgnore 처리를 통해 무한 루프 발생을 해결해야 함
-     *              => Hibernate5Module
+     *              => 1. Hibernate5Module (하지만, Response - DTO 를 생성하여 관리하는 것이 더 나은 설계)
      */
 
+    /* V1. Entity 직접 노출 방법 */
     @GetMapping("/api/v1/simple-orders")
     public List<Order> ordersV1() {
-        List<Order> all = orderRepository.findAll(new OrderSearch()); // 모든 주문 내역 조회
+        // Entity 지연 로딩을 위한 Hibernate5Module - Dependency 추가
+        List<Order> all = orderRepository.findAll(new OrderSearch()); // 모든 주문 내역 조회 (양방향 매핑 이슈)
+
+        // 필요한 필드만 LAZY 로 셋팅하여 조회
+        for (Order order : all) {
+            order.getMember().getName();        // Lazy 강제 초기화
+            order.getDelivery().getAddress();   // Lazy 강제 초기화
+        }
 
         return all;
     }
